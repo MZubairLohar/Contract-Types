79ee83ebe1a59f603b58c2ded748534c33dbcf8b
MZubairLohar <zubairgituser@gmail.com>
2020-07-13 19:29:13 +0500

    real Estate project
    complete erc721

--- contract types/ERC721/erc721 ourtype/ERC721.sol
+++ contract types/ERC721/erc721 ourtype/ERC721.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.6.0;
 
 import "./ERC165.sol";
-//import "./SafeMath.sol";
+import "./SafeMath.sol";
 import "./IERC721.sol";
 
 
  contract ERC721 is ERC165, IERC721{
-  //   using SafeMath for uint256;
+     using SafeMath for uint256;
     
     
     
@@ -36,7 +36,7 @@ import "./IERC721.sol";
     string private _symbol;
     uint256 private _totalSupply;
     bytes4 private constant _INTERFACE_ID_ERC721 = 0x80ac58cd;
-    
+    uint256 private tokenIdCounter;
     
     
     
@@ -49,7 +49,8 @@ import "./IERC721.sol";
     mapping ( uint256 => address ) private _tokenOwners;
     mapping ( address => mapping ( uint256 => uint256)) private _ownerTokenIndex;
     mapping ( uint256 => address ) private _tokenApprovals;
-    mapping ( address => mapping(address => bool)) private _operatorApproval;
+    mapping ( address => mapping(address => bool)) private _operatorApprovals;
+    mapping ( uint256 => string ) private _tokenURIs;
     
     
     
@@ -92,9 +93,9 @@ import "./IERC721.sol";
         return _ownerTokenIndex[owner][index];
     }
     
-     function setApprovedForAll(address operator, bool approved) public view override returns(bool) {
-        require( operator != msg.sender, " ");
-        _operatorApproval[msg.sender][operator];
+     function setApprovalForAll(address operator, bool approved) public virtual override {
+        require( operator != msg.sender, " approve to caller");
+        _operatorApprovals[msg.sender][operator];
         emit ApprovalForAll(msg.sender, operator, approved );
     }
     
@@ -113,29 +114,33 @@ import "./IERC721.sol";
     }
     
     
-    function isApprovedForAll( address owner, address spender) internal view returns(bool){
-         return _operatorApproval[owner][spender];
-    } 
+    
     
     function _isApproveOrOwner(address spender, uint256 tokenId) internal view returns(bool){
         require(_exist(tokenId),"invalid token ID");
-        address owner = ownerOf[tokenId];
+        address owner = ownerOf(tokenId);
         return( spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
     }
+    
+    
+    
+    
+    
+    
     
     
     //trasnfer functions
     
     function approve(address to, uint256 tokenId) public virtual override{
-        address owner = ownerOf[tokenId];
+        address owner = ownerOf(tokenId);
         require(owner != to, "approval to current owner");
-        require(owner == msg.sender || isApprovedForAll(owner. msg.sender), "approve caller is not owner approve for all");
+       // require(owner == msg.sender || isApprovedForAll(owner, msg.sender), "approve caller is not owner approve for all");
         _approve(to, tokenId);
     }
     
    
-    function isApprovedForAll(address owner, address operator) public virtual override {
-         return _operatorApproval[owner][operator];
+    function isApprovedForAll(address owner, address operator) public view override returns (bool){
+         return _operatorApprovals[owner][operator];
     }
     
     function transfer(address to, uint256 tokenId) public virtual{
@@ -145,7 +150,7 @@ import "./IERC721.sol";
         
     }
     
-    function transferFrom(address from, address to, uint256 tokenId) public virtual override{
+    function transferFrom(address from, address to, uint256 tokenId) public override{
         require(_isApproveOrOwner(from, tokenId), "approve caller is not owner");
         _transfer(from, to, tokenId);
     }
@@ -156,50 +161,87 @@ import "./IERC721.sol";
    
    
     // private or internal functions
+    
+    
+    
+    
     function _approve(address to,uint256 tokenId) private {
         _tokenApprovals[tokenId] = to;
         emit Approval(ownerOf(tokenId), to, tokenId);
     }
     
     
-    function _mint(address to, uint256 tokenId) internal virtual{
+    function _mint(address to, uint256 tokenId) internal {
         require( to != address(0), "Enter valid address");
-        totalSupply = totalSupply.add(1);
+       _totalSupply = _totalSupply.add(1);
         _addToken(to, tokenId);
         emit Transfer(address(0), to, tokenId);
     }
     
     
     function burn(uint256 tokenId) internal virtual{
-        owner = ownerOf[tokenId];
+        address owner = ownerOf(tokenId);
         _approve(address(0),tokenId);
-        if( bytes(_tokenURI[tokenId]).length != 0){
-         delete _tokenURI[tokenId];
+        if( bytes(_tokenURIs[tokenId]).length != 0){
+         delete _tokenURIs[tokenId];
         }
-        deleteToken (owner,tokenId);
+        _deleteToken(owner,tokenId);
         _totalSupply = _totalSupply.sub(1);
+        emit Transfer(owner, address(0), tokenId);
+    }
+    
+    function _transfer(address from, address to, uint256 tokenId) internal virtual{
+        require(tokenId > 0, "invalid token Id");
+        require(ownerOf(tokenId) == from, "non existing token");
+        require(to != address(0), "invalid address");
+        _approve(address(0), tokenId);
+        _deleteToken(from, tokenId);
+        _addToken(to, tokenId);
+        emit Transfer(from, to, tokenId);
+    }
+    
+    function _addToken(address owner, uint256 tokenId) internal virtual returns(bool success, uint256 newIndex){
+        require(!_exist(tokenId),"token ID already exist");
+        require(_tokenOwners[tokenId] != owner ,"already owner of this token");
+        _tokenOwners[tokenId] = owner;
+        _ownerTokens[owner].push(tokenId);
+        newIndex = _ownerTokens[owner].length-1;
+        _ownerTokenIndex[owner][tokenId] = newIndex;
+        success = true;
+    }
+    
+    
+    function _deleteToken(address owner, uint256 tokenId) internal virtual returns(bool success, uint256 Index){
+        require(_exist(tokenId),"token ID is non-existing");
+        require(_tokenOwners[tokenId] == owner, "not correct owner of token");
+         Index = _ownerTokenIndex[owner][tokenId];
+        if( _ownerTokens[owner].length>1){
+            uint256 lastToken = _ownerTokens[owner][_ownerTokens[owner].length-1] ; 
+            _ownerTokens[owner][Index] = lastToken;
+            _ownerTokenIndex[owner][lastToken] = Index;
+        }
+        _ownerTokens[owner].pop();
+        delete _ownerTokenIndex[owner][tokenId];
+        delete _tokenOwners[tokenId];
+        success = true;
+    }
+    
+    
+    function _setTokenURI(uint256 tokenId,string memory _tokenURI ) internal virtual{
+        require(_exist(tokenId),"non-existing token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+    
+    
+    // main logic for real state
+    
+    function registerProperty(string memory PlotNo) public {
+        tokenIdCounter = tokenIdCounter.add(1);
+        _mint(msg.sender, tokenIdCounter);
+        _setTokenURI(tokenIdCounter,PlotNo);
         
     }
     
-    
-    
-    //safemath functions
-        
-    function add(uint256 a, uint256 b) internal pure returns(uint256){
-        uint256 c = a + b;
-        require ( c >= a , " SafeMath: addition overflow");
-        return c;
-    }
-    
-    function sub(uint256 a, uint256 b) internal pure returns(uint256){
-        require ( b <= a , " SafeMath: subtraction overflow");
-        uint256 c = a - b;
-        return c;
-    }
-    function sub(uint256 a, uint256 b) internal pure returns(uint256){
-        require(sub(a,b)," subtraction flow");
-    }
-
 
 
  }
